259988f0b6369e0b74c9dc157670b173528c198c
Brendan Meehan <bmmeehan3@gmail.com>
2023-12-25 23:21:12 -0500

    successfully captured images from video stream in ImageReader

---
+++ app/src/main/java/com/example/bsecure/CameraActivity.java
@@ -0,0 +1,18 @@
+package com.example.bsecure;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CameraActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2VideoFragment.newInstance())
+                    .commit();
+        }
+    }
+}
--- app/src/main/java/com/example/bsecure/MainActivity.java
+++ app/src/main/java/com/example/bsecure/MainActivity.java
@@ -1,51 +1,72 @@
 package com.example.bsecure;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.OutputConfiguration;
+import android.hardware.camera2.params.SessionConfiguration;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.ImageReader;
+import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.util.Range;
+import android.util.Size;
+import android.view.Surface;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.video.MediaStoreOutputOptions;
-import androidx.camera.video.Quality;
-import androidx.camera.video.QualitySelector;
-import androidx.camera.video.Recorder;
-import androidx.camera.video.Recording;
-import androidx.camera.video.VideoCapture;
-import androidx.camera.video.VideoRecordEvent;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.example.bsecure.databinding.ActivityMainBinding;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
     private ActivityResultLauncher<String[]> requestPermissionLauncher;
-    private ImageCapture imageCapture;
-    private VideoCapture<Recorder> videoCapture;
-    private Recording recording;
-    private ActivityMainBinding viewBinding;
-    private ExecutorService cameraExecutor;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(viewBinding.getRoot());
+        setContentView(R.layout.activity_main);
+
         requestPermissionLauncher =
                 registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), map -> {
                     for (Map.Entry<String, Boolean> entry : map.entrySet()) {
@@ -58,89 +79,19 @@ public class MainActivity extends AppCompatActivity {
 
         PermissionCheck permChecker = new PermissionCheck(getApplicationContext(), requestPermissionLauncher);
         permChecker.checkPermissions();
+        Button cameraStart = (Button) findViewById(R.id.startCamera);
+        Button cameraTest = (Button) findViewById(R.id.cameraTest);
 
-        cameraExecutor = Executors.newSingleThreadExecutor();
-        Button startRecording = (Button) findViewById(R.id.video_capture_button);
-        viewBinding.videoCaptureButton.setEnabled(true);
-        viewBinding.videoCaptureButton.setOnClickListener(new View.OnClickListener(){
+        cameraTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                captureVideo();
+                Intent goToCamera = new Intent(getApplicationContext(), CameraActivity.class);
+                startActivity(goToCamera);
             }
         });
 
-        try {
-            startCamera();
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void takePhoto() {}
-    private void captureVideo() {
-        if (videoCapture == null) {
-            return;
-        }
-        viewBinding.videoCaptureButton.setEnabled(false);
-
-        Recording curRecording = recording;
-        if (curRecording != null) {
-            curRecording.stop();
-            recording = null;
-            return;
-        }
-        String name = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
-                .format(System.currentTimeMillis());
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
-        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4");
-
-        MediaStoreOutputOptions mediaStoreOutputOptions = new MediaStoreOutputOptions.Builder(getContentResolver(), MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
-                .setContentValues(contentValues).build();
-        recording = videoCapture.getOutput()
-                .prepareRecording(this, mediaStoreOutputOptions)
-                .start(ContextCompat.getMainExecutor(this), recordEvent -> {
-                    if (recordEvent instanceof VideoRecordEvent.Start) {
-                        viewBinding.videoCaptureButton.setEnabled(true);
-                        viewBinding.videoCaptureButton.setText(getString(R.string.stop_capture));
-                    }
-                    if (recordEvent instanceof VideoRecordEvent.Finalize) {
-                        if (!((VideoRecordEvent.Finalize) recordEvent).hasError()) {
-                            String msg = "Video capture succeeded: " + ((VideoRecordEvent.Finalize) recordEvent).getOutputResults().getOutputUri();
-                            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-                        } else {
-                            if (recording != null) {
-                                recording.close();
-                                recording = null;
-                            }
-                        }
-                        viewBinding.videoCaptureButton.setText(getString(R.string.start_capture));
-                        viewBinding.videoCaptureButton.setEnabled(true);
-                    }
-                });
 
     }
-    private void startCamera() throws ExecutionException, InterruptedException {
-        ProcessCameraProvider cameraProviderFuture = ProcessCameraProvider.getInstance(this).get();
-        Preview preview = new Preview.Builder().build();
-        preview.setSurfaceProvider(viewBinding.viewFinder.getSurfaceProvider());
-        Recorder recorder = new Recorder.Builder()
-                .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
-                        .build();
-        videoCapture = VideoCapture.withOutput(recorder);
-        cameraProviderFuture.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview);
-        try {
-            cameraProviderFuture.unbindAll();
-            cameraProviderFuture.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview, videoCapture);
-        } catch (Exception e) {
 
-        }
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        cameraExecutor.shutdown();
-    }
+
 }
